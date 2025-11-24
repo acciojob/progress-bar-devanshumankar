2eda5ce24f06182cdc430493a07552704609501b
Devanshu Chandu Mankar <158685271+devanshumankar@users.noreply.github.com>
2025-11-24 16:36:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const nextButton = document.querySelector("#next");
+        let currentActiveElement = document.querySelector("#circle-1");
+        const prevButton = document.querySelector("#prev");
+        nextButton.addEventListener("click", () => {
+            if (currentActiveElement.id == "circle-5") nextButton.style.display = "none";
+            else {
+                prevButton.style.display = "inline"
+                currentActiveElement.nextElementSibling.classList.add("active");
+                currentActiveElement = currentActiveElement.nextElementSibling;
+            }
+
+        })
+        prevButton.addEventListener("click", () => {
+            if(currentActiveElement.id!="circle-1"){
+                nextButton.style.display="inline"
+                currentActiveElement.classList.remove("active");
+                currentActiveElement = currentActiveElement.previousElementSibling
+            }
+            else{
+                prevButton.style.display="none"
+            }
+        })
